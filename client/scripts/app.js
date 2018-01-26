dd76f55a31ed784dec1308c8de87b11a23668c25
Dayan Perera <dayannp@gmail.com>
2018-01-26 14:25:40 -0800

    setup

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,23 @@
 // YOUR CODE HERE:
+var message = {
+  username: 'dayan',
+  text: 'Hi Every One',
+  roomname: 'hrsf-90'
+};
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+    console.log(data);
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
+
