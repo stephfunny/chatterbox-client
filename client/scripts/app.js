83689bb113c7cc6d21b929a830abd3ee66206e73
stephfunny <contactstephanief@gmail.com>
2018-01-26 20:46:59 -0800

    added messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,23 +1,104 @@
 // YOUR CODE HERE:
-var message = {
-  username: 'dayan',
-  text: 'Hi Every One',
-  roomname: 'hrsf-90'
+// var message = {
+//   username: 'dayan',
+//   text: 'Hi Every One',
+//   roomname: 'hrsf-90'
+// };
+
+
+// $.get( "http://parse.sfm6.hackreactor.com/chatterbox/classes/messages", function(data, status) { 
+// } );
+
+
+var app = {
+  
+  init : function() {
+   
+  },
+  
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+  
+  send : function(data) { 
+
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'POST',
+      data: data,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+    
+  },
+  
+  fetch : function() { 
+
+    $.get( this.server, function(data, status) { 
+      app.displayMessages(data);
+    });
+  },
+  
+  clearMessages: function() {
+    $('#chats').empty();
+  },
+  
+
+  renderMessage: function(message) {
+   $('#chats').append("<tr><td>" + message['username']+"</td><td>"+message['text']+"</td><td>"+message['roomname']+"</td> </tr>");
+  },  
+  
+  renderRoom: function(roomName) {
+    //data['results'];
+    $('#roomSelect').append("<option value=" + roomName + ">" + roomName + "</option>");
+  },
+  
+  displayMessages: function (data) {
+    var messageString = '';
+    var roomNames = [];
+    var roomString;
+    data['results'].forEach( (message) =>{
+      messageString += "<tr><td>" + message['username']+"</td><td>"+message['text']+"</td><td>"+message['createdAt']+"</td> </tr>";
+      roomNames.push(message['roomname']);
+    });
+    roomNames = _.uniq(roomNames);
+    roomNames.forEach(function(item) {
+      
+      if (item !=='' && item !== undefined && !item.includes('<')) {
+        roomString += "<option value=" + item + ">" + item + "</option>";
+      }
+
+    });
+    
+    $('#room').append(roomString);
+    $('.chatMessages').append(messageString);
+  }
+  
+  
 };
 
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-    console.log(data);
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
+
+
+
+
+
+$(document).ready(function() {
+     app.init();
+     app.fetch()
+     
+  $('#clear-messages').click( function() {
+      app.clearMessages();
+ });
+  
+  
 });
 
+          // console.log('Id',message['objectId']);
+          // console.log('user name ->',message['username']);
+          // console.log('room name ->',message['roomname']);
+          // console.log('Text',message['text']);
+          // console.log('Created At',message['createdAt']);
--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -61,6 +61,8 @@ describe('chatterbox', function() {
         app.fetch();
         expect($.ajax.calledOnce).to.be.true;
         ajaxUrl = typeof $.ajax.args[0][0] === 'string' ? $.ajax.args[0][0] : $.ajax.args[0][0].url;
+        console.log('App Server',app.server)
+     
         expect(ajaxUrl).to.equal(app.server);
         done();
       });
